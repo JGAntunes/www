ee4b4aa9e2f6fa57d7c4b80b7a8cb964859e44df
JGAntunes <j.goncalo.antunes@ist.utl.pt>
2014-08-08 12:12:41 -0400

    index page and public requests working

--- server/index.js
+++ server/index.js
@@ -7,11 +7,38 @@ var serverOptions = {
         engines: {
             html: Handlebars
         },
-        path: Path.join(__dirname, 'templates')
+        path: Path.join(__dirname, '../views')
     }
 };
 
 var server = new Hapi.Server(3000, serverOptions);
+
+server.route({
+  method: 'GET', 
+  path: '/public/{path*}', 
+  config: {
+    handler: {
+      directory: { 
+        path: './public/', 
+        listing: false, 
+        index: true 
+      }
+    }
+  }
+});
+
+server.route({
+    method: 'GET',
+    path: '/{path*}',
+    handler: function (request, reply) {
+        if(request.params.path == ''){
+          reply.view('index');
+        }
+        else{
+          reply.view('index');
+        }
+    }
+});
 
 server.start(function () {
     console.log('Server running at:', server.info.uri);
